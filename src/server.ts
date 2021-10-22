84163a03010eb8d5e88da55077e1d16f507ab024
Eugene Barannik <wellfail@atomicwallet.io>
2021-10-22 22:19:02 +0300

    feat: add express and default route

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('test')
+})
+
 export const server = () => {
-  
+  app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server started at: http://localhost:${process.env.PORT}`)
+  })
 }
