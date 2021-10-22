d0683f7d525dbe404f97f35cefcc6b3d31db313d
Eugene Barannik <wellfail@atomicwallet.io>
2021-10-23 00:31:15 +0300

    mvp code tester

---
+++ code-tester/handler.js
@@ -0,0 +1,19 @@
+module.exports.hello = async (event) => {
+  const { questionId, args, expectedResponse, code, userId } = JSON.parse(event.body)
+
+  // eslint-disable-next-line no-eval
+  const testFunction = eval(code)
+  const response = testFunction(args)
+
+  const isExpectedResponse = response === expectedResponse
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      response,
+      isExpectedResponse,
+      questionId,
+      userId,
+    }),
+  }
+}
